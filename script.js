05dcc8d4a38c725827a999326103b30fdd897dfd
Oliver <ojhowe95@gmail.com>
2020-07-24 20:33:54 -0600

    Made site

---
+++ script.js
@@ -0,0 +1,57 @@
+const speed = 50;
+var isDone = false;
+
+window.onload = async function () {
+    const input = document.getElementsByClassName("input")[document.getElementsByClassName("input").length - 1];
+    let elements = document.body.getElementsByClassName("group");
+    for (let i = 0; i < elements.length; i++) {
+        let question = elements[i].getElementsByClassName("question")[0];
+        let answer = elements[i].getElementsByClassName("response")[0];
+        let text = question.innerHTML;
+        question.innerHTML = "<span class='header'>Oliver@CarelessHippo:~$</span>  ";
+        question.style.display = "block";
+        for (let j = 0; j < text.length; j++) {
+            question.innerHTML = question.innerHTML.slice(0, question.innerHTML.length - 1);
+            question.innerHTML += text[j];
+            question.innerHTML += "▮"
+            await sleep(speed);
+        }
+        await sleep(400);
+        answer.style.display = "block";
+        await sleep(100);
+        question.innerHTML = question.innerHTML.slice(0, question.innerHTML.length - 1);
+    }
+    input.innerHTML = "<span class='header'>Oliver@CarelessHippo:~$</span> ▮";
+    input.style.display = "block";
+    isDone = true;
+}
+
+document.onkeypress = function (e) {
+    const input = document.getElementsByClassName("input")[document.getElementsByClassName("input").length - 1];
+    if (isDone) {
+        if (e.keyCode == 13) {
+            return;
+        }
+
+        input.innerHTML = input.innerHTML.slice(0, input.innerHTML.length - 1);
+        input.innerHTML += e.key;
+        input.innerHTML += "▮";
+    }
+}
+
+function key(e) {
+    const input = document.getElementsByClassName("input")[document.getElementsByClassName("input").length - 1];
+    if (e.keyCode == 8) {
+        input.innerHTML = input.innerHTML.slice(0, input.innerHTML.length - 2)
+        input.innerHTML += "▮"
+    }
+}
+
+
+document.addEventListener("keydown", key)
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
